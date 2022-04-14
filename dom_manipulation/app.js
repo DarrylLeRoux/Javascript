272b933345a0c9e084dc54044f5dc315db03ad6d
Darryl Le Roux <devdarryl@outlook.com>
2022-04-14 21:27:42 +0200

    Added styles for winning

--- dom_manipulation/app.js
+++ dom_manipulation/app.js
@@ -11,16 +11,44 @@
 
 // Get random Number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 document.querySelector('.number').textContent = secretNumber;
+
+const scoreEl = document.querySelector('.score');
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
+  // When there is no number input by user
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+    // When the user wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-  } else if (guess !== secretNumber) {
-    document.querySelector('.message').textContent = '❌ Try Again!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When the number is higher than the correct number
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      scoreEl.textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost the Game!';
+      scoreEl.textContent = 0;
+    }
+
+    // When the number is lower than the correct number
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      scoreEl.textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost the Game!';
+      scoreEl.textContent = 0;
+    }
   }
 });
