0f3cf2671e8b25ee87eed2fcd05abbf32d8a037e
Darryl Le Roux <devdarryl@outlook.com>
2022-04-14 20:40:48 +0200

    Added correct number logic

--- dom_manipulation/app.js
+++ dom_manipulation/app.js
@@ -4,15 +4,23 @@
 
 // document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-// document.querySelector('.number');
 // document.querySelector('.score');
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
+
+// Get random Number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess !== secretNumber) {
+    document.querySelector('.message').textContent = '❌ Try Again!';
   }
 });
