aad1e65ab3d8dac834c4987a6249167c45474d6a
Darryl Le Roux <devdarryl@outlook.com>
2022-04-14 22:15:16 +0200

    keep highscore implemented

--- dom_manipulation/app.js
+++ dom_manipulation/app.js
@@ -2,7 +2,9 @@
 
 // Get random Number
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -15,8 +17,14 @@ document.querySelector('.check').addEventListener('click', () => {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // When the number is higher than the correct number
   } else if (guess > secretNumber) {
@@ -34,10 +42,10 @@ document.querySelector('.check').addEventListener('click', () => {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
       score--;
-      scoreEl.textContent = score;
+      document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = '💥 You Lost the Game!';
-      scoreEl.textContent = 0;
+      document.querySelector('.score').textContent = 0;
     }
   }
 });
