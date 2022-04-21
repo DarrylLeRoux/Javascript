eff78abd8cb5631f8ba396ac9c654d135d50990f
Darryl Le Roux <devdarryl@outlook.com>
2022-04-21 17:50:13 +0200

    Formatting

--- data_structures/script.js
+++ data_structures/script.js
@@ -357,48 +357,7 @@ BONUS: Create an object called 'scorers' which contains the names of the players
         Hummels: 1,
         Lewandowski: 2
       }
-*/
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels", "Jackie"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-console.log(game.scored.entries());
+
 // 1.
 for (let [goal, scored] of game.scored.entries()) {
   console.log(`Goal ${goal + 1} was scored by: ${scored}`);
@@ -418,3 +377,4 @@ for (const [team, odd] of Object.entries(game.odds)) {
   const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
   console.log(`Odds of ${teamStr} winning: ${odd}`);
 }
+*/
