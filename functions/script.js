55a646f69778dad48dfe48ae8a24181bc9170ee6
Darryl Le Roux <devdarryl@outlook.com>
2022-05-11 21:09:32 +0200

    Added last closure example

--- functions/script.js
+++ functions/script.js
@@ -257,6 +257,28 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
+console.dir(booker);
+
+let f;
+const g = function () {
+  const a = 23;
+  // Stays in the lexical scope due to closures
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+h();
+f();
 /* // Coding Challenge #1
 Let's build a simple poll app!
 A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
@@ -298,14 +320,9 @@ This is more of a thinking challenge than a coding challenge 🤓
 Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
 And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
 GOOD LUCK 😀
-
-
+*/
 
 (function () {
   const header = document.querySelector('h1');
   header.style.color = 'red';
-  document.querySelector('body').addEventListener('click', function () {
-    header.style.color = 'blue';
-  });
 })();
-*/
