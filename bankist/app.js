43639c978c42e138a129023b7f03afabdfc5d019
Darryl Le Roux <devdarryl@outlook.com>
2022-05-13 20:57:52 +0200

    forEach on Maps and Sets

--- bankist/app.js
+++ bankist/app.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -134,3 +128,23 @@ for (const [i, move] of movements.entries()) {
     console.log(`Transaction ${i + 1}: You withdrew $${Math.abs(move)}`);
   }
 }
+
+// Map and Sets with forEach
+
+// Maps
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}, ${map}`);
+});
+
+// Sets
+const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'EUR']);
+
+currenciesUnique.forEach((currency) => {
+  console.log(`${currency}`);
+});
