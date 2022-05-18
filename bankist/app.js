a24e7973496c3c400e519b3ea9e7668cf7442815
Darryl Le Roux <devdarryl@outlook.com>
2022-05-18 21:13:52 +0200

    added filter for deposits

--- bankist/app.js
+++ bankist/app.js
@@ -103,14 +103,15 @@ function createUsernames(accs) {
 
 createUsernames(accounts);
 
-// Loop over all usernames for initials
-
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+const deposits = movements.filter((mov) => {
+  return mov > 0;
+});
+console.log(deposits);
 /////////////////////////////////////////////////
 // Array methods
 /////////////////////////////////////////////////
