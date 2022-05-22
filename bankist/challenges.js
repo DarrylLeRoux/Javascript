c5bf4b0d9b0411fca997e29b02bc5cf53253ba23
Darryl Le Roux <devdarryl@outlook.com>
2022-05-22 21:09:52 +0200

    Completed challenge

--- bankist/challenges.js
+++ bankist/challenges.js
@@ -6,11 +6,14 @@
 const calcAverageHumanAge = function (ages) {
   //calculate dog age in human age
   const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
-  console.log(humanAges);
+  // Exclude all dogs that are less than 18 human years old
+  const adults = humanAges.filter((age) => {
+    return age >= 18;
+  });
+  // Calculate the average human age of all adult dogs
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  return average;
 };
 
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-
-// Exclude all dogs that are less than 18 human years old
-
-// Calculate the average human age of all adult dogs
+const avg = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+console.log(avg);
