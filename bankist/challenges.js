fcb73e87a62ed87ab3cb61d7437a1ba57ca3c0ff
Darryl Le Roux <devdarryl@outlook.com>
2022-05-22 20:43:01 +0200

    Added test variables

--- bankist/challenges.js
+++ bankist/challenges.js
@@ -2,3 +2,15 @@
 
 // TEST DATA 1: [5,2,4,1,15,8,3]
 // TEST DATA 2: [16,6,10,5,6,1,4]
+const calcAverageHumanAge = function (ages) {
+  //calculate dog age in human age
+  if (dogAge <= 2) {
+    humanAge = 2 * dogAge;
+  } else if (dog > 2) {
+    humanAge = 16 + dogAge * 4;
+  }
+};
+
+// Exclude all dogs that are less than 18 human years old
+
+// Calculate the average human age of all adult dogs
