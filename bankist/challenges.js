23589b3f1db1b1edeed976d4c0c40c0cbf920a91
Darryl Le Roux <devdarryl@outlook.com>
2022-05-22 20:57:21 +0200

    First challenge section completed

--- bankist/challenges.js
+++ bankist/challenges.js
@@ -2,14 +2,14 @@
 
 // TEST DATA 1: [5,2,4,1,15,8,3]
 // TEST DATA 2: [16,6,10,5,6,1,4]
+
 const calcAverageHumanAge = function (ages) {
   //calculate dog age in human age
-  if (dogAge <= 2) {
-    humanAge = 2 * dogAge;
-  } else if (dog > 2) {
-    humanAge = 16 + dogAge * 4;
-  }
+  const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+  console.log(humanAges);
 };
+
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 
 // Exclude all dogs that are less than 18 human years old
 
