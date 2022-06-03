fe2b3e489d234b03fdd95b4613f68c57e50119bb
Darryl Le Roux <devdarryl@outlook.com>
2022-06-03 21:27:41 +0200

    added join

--- bankist/app.js
+++ bankist/app.js
@@ -722,14 +722,15 @@ console.log(bankDeposits, bankWithdrawals);
 
 // Covert string to titlecase
 const convertTitleCase = (title) => {
-  const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
 
   const titleCase = title
     .toLowerCase()
     .split(' ')
     .map((word) =>
       exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
-    );
+    )
+    .join(' ');
   return titleCase;
 };
 console.log(convertTitleCase('this is a nice title'));
