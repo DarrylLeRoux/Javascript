92eb07655b49692bebe55d1451b971fe176e98d0
Darryl Le Roux <devdarryl@outlook.com>
2022-06-12 19:28:01 +0200

    Styles attributes and classes

--- advanced_dom/script.js
+++ advanced_dom/script.js
@@ -45,13 +45,11 @@ const header = document.querySelector(".header");
 
 // If the variable is crated first storing a nodelist - sections, then even if it is deleted, it will still be printed
 const allSections = document.querySelectorAll(".section");
-console.log(allSections);
 
 document.getElementById("section--1");
 
 // HTML collection (Live collection - if the DOM changes, this is updated)
 const allButtons = document.getElementsByTagName("button");
-console.log(allButtons);
 
 document.getElementsByClassName("btn");
 
@@ -76,3 +74,43 @@ header.prepend(message);
 document.querySelector(".btn--close-cookie").addEventListener("click", () => {
   message.remove();
 });
+
+////////////////////////////////////////////////////////////////////////
+// STYLES, ATTRIBUTES AND CLASSES
+////////////////////////////////////////////////////////////////////////
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+console.log(getComputedStyle(message).color); // get the style of an element
+console.log(getComputedStyle(message).width); // get the style of an element
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+// CSS Variables
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// Attributes
+
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt, logo.src);
+// Set attributes
+logo.setAttribute("company", "Jonas");
+// Get attribute
+console.log(logo.getAttribute("company")); // "Jonas"
+console.log(logo.getAttribute("src")); // "img/logo.png"
+const navLink = document.querySelector(".nav__link--btn");
+console.log(navLink.href); // absolute (http://127.0.0.1:5501/advanced_dom/#)
+console.log(navLink.getAttribute("href")); //relevant (#)
+
+// Data Attributes
+document.querySelector(".nav__logo").setAttribute("data-version-number", "3.0");
+console.log(logo.dataset.versionNumber); // 3.0
+
+// Classes
+logo.classList.add("c");
+logo.classList.remove("c");
+logo.classList.toggle("c");
+logo.classList.contains("c");
+
+// Don't use - overrides the original
+logo.className = "Jonas";
