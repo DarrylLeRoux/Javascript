9b3099b270e6975901f45096d89210aa8b936cfa
Darryl Le Roux <devdarryl@outlook.com>
2022-06-13 21:28:32 +0200

    Stop propogation on events

--- advanced_dom/script.js
+++ advanced_dom/script.js
@@ -162,6 +162,21 @@ const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
 // Use random number for RGB Values
-const randomColor = () => {
+const randomColor = () =>
   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
-};
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(e.target);
+
+  // stop propogation
+  e.stopPropagation(); // fixes problems in many handlers, not to use in general
+});
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(e.target, "LINKS");
+});
+document.querySelector(".nav").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(e.target, "HEADER");
+});
